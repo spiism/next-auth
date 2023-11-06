516272f25e70bfc85255ff7ec3f1a518e24f6a0c
spiism <sp.iism@gmail.com>
2023-11-07 03:07:20 +1300

    add json web token

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,6 @@
-import * as bcrypt from "bcrypt";
+import { signJwtAccessToken } from "@/lib/jwt";
 import prisma from "@/lib/prisma";
+import * as bcrypt from "bcrypt";
 
 interface RequestBody {
   username: string;
@@ -16,6 +17,11 @@ export async function POST(request: Request) {
 
   if (user && (await bcrypt.compare(body.password, user.password))) {
     const { password, ...userWithoutPass } = user;
-    return new Response(JSON.stringify(userWithoutPass));
+    const accessToken = signJwtAccessToken(userWithoutPass);
+    const result = {
+      ...userWithoutPass,
+      accessToken,
+    };
+    return new Response(JSON.stringify(result));
   } else return new Response(JSON.stringify(null));
 }
